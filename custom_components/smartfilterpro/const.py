6617d4e3ad73be8d852178c42d118d78ae7d0cf3
smartfilterpro <eric@smartfilterpro.com>
2025-09-02 11:21:16 -0400

    Update const.py

--- custom_components/smartfilterpro/const.py
+++ custom_components/smartfilterpro/const.py
@@ -4,4 +4,4 @@ CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"  # optional if using PKCE
 CONF_API_BASE = "api_base"
 CONF_WEBHOOK_ID = "webhook_id"
-DEFAULT_API_BASE = "https://api.smartfilterpro.com/v1"
+DEFAULT_API_BASE = "https://smartfilterpro-scaling.bubbleapps.io/version-test/api/1.1/wf/home_assistant/initialize"
