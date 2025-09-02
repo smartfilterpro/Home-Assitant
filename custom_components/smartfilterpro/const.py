e1d7b762e1702062a5e13a98aa62854ed96a4a6d
smartfilterpro <eric@smartfilterpro.com>
2025-09-02 11:06:39 -0400

    Update const.py

--- custom_components/smartfilterpro/const.py
+++ custom_components/smartfilterpro/const.py
@@ -1,1 +1,7 @@
-
+DOMAIN = "smartfilterpro"
+PLATFORMS = ["sensor"]
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"  # optional if using PKCE
+CONF_API_BASE = "api_base"
+CONF_WEBHOOK_ID = "webhook_id"
+DEFAULT_API_BASE = "https://api.smartfilterpro.com/v1"
