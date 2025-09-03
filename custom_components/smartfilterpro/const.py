87b66a8890af39146ec484e948233cf26415af53
smartfilterpro <eric@smartfilterpro.com>
2025-09-02 21:03:58 -0400

    Update const.py

--- custom_components/smartfilterpro/const.py
+++ custom_components/smartfilterpro/const.py
@@ -1,7 +1,24 @@
 DOMAIN = "smartfilterpro"
-PLATFORMS = ["sensor"]
-CONF_CLIENT_ID = "client_id"
-CONF_CLIENT_SECRET = "client_secret"  # optional if using PKCE
+
+CONF_USER_ID = "user_id"
+CONF_HVAC_ID = "hvac_id"
+CONF_ENTITY_ID = "entity_id"
+
 CONF_API_BASE = "api_base"
-CONF_WEBHOOK_ID = "webhook_id"
-DEFAULT_API_BASE = "https://smartfilterpro-scaling.bubbleapps.io/version-test/api/1.1/wf/home_assistant/initialize"
+CONF_POST_PATH = "post_path"
+CONF_RESOLVER_PATH = "resolver_path"
+CONF_DATA_OBJ_URL = "data_obj_url"
+
+# NEW
+CONF_RESET_PATH = "reset_path"
+
+DEFAULT_API_BASE = "https://smartfilterpro.com/version-test/api/1.1/wf"
+DEFAULT_POST_PATH = "ha_telemetry"
+DEFAULT_RESOLVER_PATH = "ha_resolve_thermostat_obj"
+# NEW (your Bubble workflow that resets totals)
+DEFAULT_RESET_PATH = "ha_reset_filter"
+
+# include button platform
+PLATFORMS = ["sensor", "button"]
+
+STORAGE_KEY = "session"
